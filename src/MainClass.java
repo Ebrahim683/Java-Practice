0d597886388e236f0c4faf0b8c644685c1d7fa5d
Ebrahim683 <ebrahimrahman20@gmail.com>
2022-01-06 18:01:13 +0600

    Linear Search & Max, Min Number from array moved to another class

--- src/MainClass.java
+++ src/MainClass.java
@@ -7,53 +7,13 @@ public class MainClass {
 
         int[] numbers = {1, 56, 87, 96, 5, 41, 2, 54, 98, 65};
         int target = 2;
-//        System.out.println(linearSearch(numbers, target));
-        System.out.println(linearSearchElement(numbers, target) + " Found in index " + linearSearchIndex(numbers, target));
 
-        System.out.println("Minimum number is " + minimumNumber(numbers));
-        System.out.println("Maximum number is " + maximumNumber(numbers));
+        Numbers numberClass = new Numbers();
+        System.out.println(numberClass.linearSearchElement(numbers, target) + " Found in index " + numberClass.linearSearchIndex(numbers, target));
+
+        System.out.println("Minimum number is " + numberClass.minimumNumber(numbers));
+        System.out.println("Maximum number is " + numberClass.maximumNumber(numbers));
     }
 
-    public static int linearSearchIndex(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            int element = numbers[i];
-            if (element == target) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
-    public static int linearSearchElement(int[] numbers, int target) {
-        boolean found = false;
-        for (int number : numbers) {
-            if (number == target) {
-                found = true;
-                break;
-            }
-        }
-        if (found) {
-            return target;
-        } else return -1;
-    }
-
-    public static int minimumNumber(int[] numbers) {
-        int minimum = numbers[0];
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] < minimum) {
-                minimum = numbers[i];
-            }
-        }
-        return minimum;
-    }
-
-    public static int maximumNumber(int[] numbers) {
-        int max = numbers[0];
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
-            }
-        }
-        return max;
-    }
 }
